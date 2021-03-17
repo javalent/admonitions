275096a979fc93a7c2b3cd2297e8cf7e8cfa39e3
valentine195 <38669521+valentine195@users.noreply.github.com>
2021-03-17 14:32:19 -0400

    0.0.4
    - Fixed issue where undefined code blocks were sneaking through

--- src/main.ts
+++ src/main.ts
@@ -17,34 +17,40 @@ export default class Admonition extends Plugin {
             (k) => `language-${k}`
         );
 
-        codeBlocks = Array.prototype.map.call(
-            codeBlocks,
-            (element: HTMLElement): HTMLElement => {
-                const classList = Array.prototype.filter.call(
-                    element.classList,
-                    (cls: string) => classMap.includes(cls)
-                );
-                if (classList.length) return element;
-            }
-        );
+        codeBlocks = Array.prototype.map
+            .call(
+                codeBlocks,
+                (element: HTMLElement): HTMLElement => {
+                    if (element) {
+                        const classList = Array.prototype.filter.call(
+                            element.classList,
+                            (cls: string) => classMap.includes(cls)
+                        );
+                        if (classList.length) return element;
+                    }
+                }
+            )
+            .filter((b: HTMLElement) => b);
         if (!codeBlocks.length) return;
 
         codeBlocks.forEach((block) => {
-            let classType = Array.prototype.find.call(
-                block.classList,
-                (cls: string) => classMap.includes(cls)
-            );
-            if (!classType) return;
-            let type =
-                ADMONITION_MAP[classType.split("language-").pop().trim()];
-            if (!type) return;
-            const {
-                title = type[0].toUpperCase() + type.slice(1).toLowerCase(),
-                content = block.innerText
-            } = Object.fromEntries(
-                block.innerText.split("\n").map((l) => l.split(": "))
-            );
-            this.buildAdmonition(block.parentElement, type, title, content);
+            if (block) {
+                let classType = Array.prototype.find.call(
+                    block.classList,
+                    (cls: string) => classMap.includes(cls)
+                );
+                if (!classType) return;
+                let type =
+                    ADMONITION_MAP[classType.split("language-").pop().trim()];
+                if (!type) return;
+                const {
+                    title = type[0].toUpperCase() + type.slice(1).toLowerCase(),
+                    content = block.innerText
+                } = Object.fromEntries(
+                    block.innerText.split("\n").map((l) => l.split(": "))
+                );
+                this.buildAdmonition(block.parentElement, type, title, content);
+            }
         });
     }
     buildAdmonition(
