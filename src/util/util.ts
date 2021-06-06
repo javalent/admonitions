79030e8f09dbf83c3e0f62b8d6a6ce6ce405f25b
valentine195 <jvalentine@abrasive-tech.com>
2021-06-05 20:39:21 -0400

    5.0.1
    - Fixed issue where collapsible by default was not working

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,179 +1,184 @@
 import { MarkdownRenderer, Notice } from "obsidian";
-import { findIconDefinition, /* icon, */ IconName } from "./icons";
 import { getIconNode } from "./icons";
 import { AdmonitionIconDefinition, INestedAdmonition } from "../@types";
 
 export function getMatches(
-    src: string,
-    from: number,
-    toMatch: string
+	src: string,
+	from: number,
+	toMatch: string
 ): INestedAdmonition {
-    const split = src.split("\n").slice(from);
-    const first = split.indexOf(split.find((l) => l == toMatch));
+	const split = src.split("\n").slice(from);
+	const first = split.indexOf(split.find((l) => l == toMatch));
 
-    let next = first + 1;
-    for (; next < split.length; next++) {
-        if (!/^(?: {2,4}|\t)+[\s\S]*?/.test(split[next])) break;
-    }
+	let next = first + 1;
+	for (; next < split.length; next++) {
+		if (!/^(?: {2,4}|\t)+[\s\S]*?/.test(split[next])) break;
+	}
 
-    let innerSrc = split.slice(first + 1, next).join("\n");
+	let innerSrc = split.slice(first + 1, next).join("\n");
 
-    const toRemove = innerSrc.split("\n")[0].match(/^(\s+)/);
-    innerSrc = innerSrc.replace(new RegExp(`^${toRemove[0] || ""}`, "gm"), "");
+	const toRemove = innerSrc.split("\n")[0].match(/^(\s+)/);
+	innerSrc = innerSrc.replace(new RegExp(`^${toRemove[0] || ""}`, "gm"), "");
 
-    return {
-        start: first + from,
-        end: next + from - 1,
-        src: innerSrc,
-        type: toMatch.split("-").pop()
-    };
+	return {
+		start: first + from,
+		end: next + from - 1,
+		src: innerSrc,
+		type: toMatch.split("-").pop(),
+	};
 }
 
 function startsWithAny(str: string, needles: string[]) {
-    for (let i = 0; i < needles.length; i++) {
-        if (str.startsWith(needles[i])) {
-            return i;
-        }
-    }
+	for (let i = 0; i < needles.length; i++) {
+		if (str.startsWith(needles[i])) {
+			return i;
+		}
+	}
 
-    return false;
+	return false;
 }
 
 export function getParametersFromSource(type: string, src: string) {
-    const keywordTokens = ["title:", "collapse:"];
+	const keywordTokens = ["title:", "collapse:"];
 
-    const keywords = ["title", "collapse"];
+	const keywords = ["title", "collapse"];
 
-    let lines = src.split("\n");
+	let lines = src.split("\n");
 
-    let skipLines = 0;
+	let skipLines = 0;
 
-    let params: { [k: string]: string } = {};
+	let params: { [k: string]: string } = {};
 
-    for (let i = 0; i < lines.length; i++) {
-        let keywordIndex = startsWithAny(lines[i], keywordTokens);
+	for (let i = 0; i < lines.length; i++) {
+		let keywordIndex = startsWithAny(lines[i], keywordTokens);
 
-        if (keywordIndex === false) {
-            break;
-        }
+		if (keywordIndex === false) {
+			break;
+		}
 
-        let foundKeyword = keywords[keywordIndex];
+		let foundKeyword = keywords[keywordIndex];
 
-        if (params[foundKeyword] !== undefined) {
-            break;
-        }
+		if (params[foundKeyword] !== undefined) {
+			break;
+		}
 
-        params[foundKeyword] = lines[i]
-            .substr(keywordTokens[keywordIndex].length)
-            .trim();
-        ++skipLines;
-    }
+		params[foundKeyword] = lines[i]
+			.substr(keywordTokens[keywordIndex].length)
+			.trim();
+		++skipLines;
+	}
 
-    let {
-        title = type[0].toUpperCase() + type.slice(1).toLowerCase(),
-        collapse = "none"
-    } = params;
+	let {
+		title = type[0].toUpperCase() + type.slice(1).toLowerCase(),
+		collapse /* = "none" */,
+	} = params;
 
-    let content = lines.slice(skipLines).join("\n");
+	let content = lines.slice(skipLines).join("\n");
 
-    /**
-     * If the admonition should collapse, but something other than open or closed was provided, set to closed.
-     */
-    if (collapse !== "none" && collapse !== "open" && collapse !== "closed") {
-        collapse = "closed";
-    }
+	/**
+	 * If the admonition should collapse, but something other than open or closed was provided, set to closed.
+	 */
+	if (
+        collapse &&
+		collapse.length &&
+		collapse !== "none" &&
+		collapse !== "open" &&
+		collapse !== "closed"
+	) {
+		collapse = "closed";
+	}
 
-    /**
-     * If the admonition should collapse, but title was blanked, set the default title.
-     */
-    if (title.trim() === "" && collapse !== "none") {
-        title = type[0].toUpperCase() + type.slice(1).toLowerCase();
-        new Notice("An admonition must have a title if it is collapsible.");
-    }
+	/**
+	 * If the admonition should collapse, but title was blanked, set the default title.
+	 */
+	if (title.trim() === "" && collapse !== "none") {
+		title = type[0].toUpperCase() + type.slice(1).toLowerCase();
+		new Notice("An admonition must have a title if it is collapsible.");
+	}
 
-    return { title, collapse, content };
+	return { title, collapse, content };
 }
 
 export /* async */ function getAdmonitionElement(
-    type: string,
-    title: string,
-    icon: AdmonitionIconDefinition,
-    color: string,
-    collapse?: string
+	type: string,
+	title: string,
+	icon: AdmonitionIconDefinition,
+	color: string,
+	collapse?: string
 ): HTMLElement {
-    let admonition,
-        titleEl,
-        attrs: { style: string; open?: string } = {
-            style: `--admonition-color: ${color};`
-        };
-    if (collapse) {
-        if (collapse === "open") {
-            attrs.open = "open";
-        }
-        admonition = createEl("details", {
-            cls: `admonition admonition-${type}`,
-            attr: attrs
-        });
-        titleEl = admonition.createEl("summary", {
-            cls: `admonition-title ${!title.trim().length ? "no-title" : ""}`
-        });
-    } else {
-        admonition = createDiv({
-            cls: `admonition admonition-${type}`,
-            attr: attrs
-        });
-        titleEl = admonition.createDiv({
-            cls: `admonition-title ${!title.trim().length ? "no-title" : ""}`
-        });
-    }
+	let admonition,
+		titleEl,
+		attrs: { style: string; open?: string } = {
+			style: `--admonition-color: ${color};`,
+		};
+	if (collapse) {
+		if (collapse === "open") {
+			attrs.open = "open";
+		}
+		admonition = createEl("details", {
+			cls: `admonition admonition-${type}`,
+			attr: attrs,
+		});
+		titleEl = admonition.createEl("summary", {
+			cls: `admonition-title ${!title.trim().length ? "no-title" : ""}`,
+		});
+	} else {
+		admonition = createDiv({
+			cls: `admonition admonition-${type}`,
+			attr: attrs,
+		});
+		titleEl = admonition.createDiv({
+			cls: `admonition-title ${!title.trim().length ? "no-title" : ""}`,
+		});
+	}
 
-    if (title && title.length) {
-        /**
-         * Title structure
-         * <div|summary>.admonition-title
-         *      <element>.admonition-title-content - Rendered Markdown top-level element (e.g. H1/2/3 etc, p)
-         *          div.admonition-title-icon
-         *              svg
-         *          div.admonition-title-markdown - Container of rendered markdown
-         *              ...rendered markdown children...
-         */
+	if (title && title.length) {
+		/**
+		 * Title structure
+		 * <div|summary>.admonition-title
+		 *      <element>.admonition-title-content - Rendered Markdown top-level element (e.g. H1/2/3 etc, p)
+		 *          div.admonition-title-icon
+		 *              svg
+		 *          div.admonition-title-markdown - Container of rendered markdown
+		 *              ...rendered markdown children...
+		 */
 
-        //get markdown
-        const markdownHolder = createDiv();
-        MarkdownRenderer.renderMarkdown(title, markdownHolder, "", null);
+		//get markdown
+		const markdownHolder = createDiv();
+		MarkdownRenderer.renderMarkdown(title, markdownHolder, "", null);
 
-        //admonition-title-content is first child of rendered markdown
-        const admonitionTitleContent = markdownHolder.children[0];
+		//admonition-title-content is first child of rendered markdown
+		const admonitionTitleContent = markdownHolder.children[0];
 
-        //get children of markdown element, then remove them
-        const markdownElements = Array.from(
-            admonitionTitleContent?.childNodes || []
-        );
-        admonitionTitleContent.innerHTML = "";
-        admonitionTitleContent.addClass("admonition-title-content");
+		//get children of markdown element, then remove them
+		const markdownElements = Array.from(
+			admonitionTitleContent?.childNodes || []
+		);
+		admonitionTitleContent.innerHTML = "";
+		admonitionTitleContent.addClass("admonition-title-content");
 
-        //build icon element
-        const iconEl = admonitionTitleContent.createDiv(
-            "admonition-title-icon"
-        );
-        if (icon && icon.name && icon.type) {
-            iconEl.appendChild(getIconNode(icon));
-        }
+		//build icon element
+		const iconEl = admonitionTitleContent.createDiv(
+			"admonition-title-icon"
+		);
+		if (icon && icon.name && icon.type) {
+			iconEl.appendChild(getIconNode(icon));
+		}
 
-        //add markdown children back
-        const admonitionTitleMarkdown = admonitionTitleContent.createDiv(
-            "admonition-title-markdown"
-        );
-        for (let i = 0; i < markdownElements.length; i++) {
-            admonitionTitleMarkdown.appendChild(markdownElements[i]);
-        }
-        titleEl.appendChild(admonitionTitleContent || createDiv());
-    }
+		//add markdown children back
+		const admonitionTitleMarkdown = admonitionTitleContent.createDiv(
+			"admonition-title-markdown"
+		);
+		for (let i = 0; i < markdownElements.length; i++) {
+			admonitionTitleMarkdown.appendChild(markdownElements[i]);
+		}
+		titleEl.appendChild(admonitionTitleContent || createDiv());
+	}
 
-    //add them to title element
+	//add them to title element
 
-    if (collapse) {
-        titleEl.createDiv("collapser").createDiv("handle");
-    }
-    return admonition;
+	if (collapse) {
+		titleEl.createDiv("collapser").createDiv("handle");
+	}
+	return admonition;
 }
