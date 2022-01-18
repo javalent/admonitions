4a941fc6e0af271bf1996183d19b6a1fd295d2b5
valentine195 <38669521+valentine195@users.noreply.github.com>
2022-01-18 08:49:02 -0500

    fix: improves live preview detection

---
+++ src/util/livepreview.ts
@@ -0,0 +1,33 @@
+import type { SelectionRange, EditorState } from "@codemirror/state";
+import {
+    editorLivePreviewField,
+    editorViewField,
+    requireApiVersion
+} from "obsidian";
+
+export const rangesInclude = (
+    ranges: readonly SelectionRange[],
+    from: number,
+    to: number
+) => {
+    for (const range of ranges) {
+        const { from: rFrom, to: rTo } = range;
+        if (rFrom >= from && rFrom <= to) return true;
+        if (rTo >= from && rTo <= to) return true;
+        if (rFrom < from && rTo > to) return true;
+    }
+    return false;
+};
+
+export const isLivePreview = (state: EditorState) => {
+    if (requireApiVersion && requireApiVersion("0.13.21")) {
+        return state.field(editorLivePreviewField);
+    } else {
+        const md = state.field(editorViewField);
+        const { state: viewState } = md.leaf.getViewState() ?? {};
+
+        return (
+            viewState && viewState.mode == "source" && viewState.source == false
+        );
+    }
+};
